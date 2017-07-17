127633a6b41c0f00a82028d2f5005c0713445d09
Rafa Hernández <rhernandez@teclib.com>
2017-07-17 19:51:06 -0400

    fix(deploy): remove local branch from deploy script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -7,11 +7,3 @@ then
     cd ..
     fastlane android alpha storepass:'$KEYSTORE' keypass:'$ALIAS'
 fi
-
-if [[ "$TRAVIS_BRANCH" == "feature/init" ]];
-then
-    cd ci
-    tar -zxvf google.tar.gz
-    cd ..
-    fastlane android alpha storepass:'$KEYSTORE' keypass:'$ALIAS'
-fi

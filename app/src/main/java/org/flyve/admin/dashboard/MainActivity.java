f53ce5f9d91097ea8630e91735772c6428210501
Rafa Hernandez <rhernandez@teclib.com>
2017-11-16 16:40:08 -0400

    feat(about): add about screen

---
+++ app/src/main/java/org/flyve/admin/dashboard/MainActivity.java
@@ -0,0 +1,13 @@
+package org.flyve.admin.dashboard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}

1e6117020d4a9b3ab57adb31baedf68039b51896
Rafa Hernandez <rhernandez@teclib.com>
2017-11-19 19:39:57 -0400

    refactor(device): add device touch helper

--- app/src/main/java/org/flyve/admin/dashboard/adapter/DeviceTouchHelper.java
+++ app/src/main/java/org/flyve/admin/dashboard/adapter/DeviceTouchHelper.java
@@ -5,10 +5,10 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 
-public class UserTouchHelper extends ItemTouchHelper.SimpleCallback {
+public class DeviceTouchHelper extends ItemTouchHelper.SimpleCallback {
     private RecyclerItemTouchHelperListener listener;
 
-    public UserTouchHelper(int dragDirs, int swipeDirs, RecyclerItemTouchHelperListener listener) {
+    public DeviceTouchHelper(int dragDirs, int swipeDirs, RecyclerItemTouchHelperListener listener) {
         super(dragDirs, swipeDirs);
         this.listener = listener;
     }
@@ -21,7 +21,7 @@ public class UserTouchHelper extends ItemTouchHelper.SimpleCallback {
     @Override
     public void onSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState) {
         if (viewHolder != null) {
-            final View foregroundView = ((UserAdapter.DataViewHolder) viewHolder).viewForeground;
+            final View foregroundView = ((DeviceAdapter.DataViewHolder) viewHolder).viewForeground;
             getDefaultUIUtil().onSelected(foregroundView);
         }
     }
@@ -30,14 +30,14 @@ public class UserTouchHelper extends ItemTouchHelper.SimpleCallback {
     public void onChildDrawOver(Canvas c, RecyclerView recyclerView,
                                 RecyclerView.ViewHolder viewHolder, float dX, float dY,
                                 int actionState, boolean isCurrentlyActive) {
-        final View foregroundView = ((UserAdapter.DataViewHolder) viewHolder).viewForeground;
+        final View foregroundView = ((DeviceAdapter.DataViewHolder) viewHolder).viewForeground;
         getDefaultUIUtil().onDrawOver(c, recyclerView, foregroundView, dX, dY,
                 actionState, isCurrentlyActive);
     }
 
     @Override
     public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
-        final View foregroundView = ((UserAdapter.DataViewHolder) viewHolder).viewForeground;
+        final View foregroundView = ((DeviceAdapter.DataViewHolder) viewHolder).viewForeground;
         getDefaultUIUtil().clearView(foregroundView);
     }
 
@@ -45,7 +45,7 @@ public class UserTouchHelper extends ItemTouchHelper.SimpleCallback {
     public void onChildDraw(Canvas c, RecyclerView recyclerView,
                             RecyclerView.ViewHolder viewHolder, float dX, float dY,
                             int actionState, boolean isCurrentlyActive) {
-        final View foregroundView = ((UserAdapter.DataViewHolder) viewHolder).viewForeground;
+        final View foregroundView = ((DeviceAdapter.DataViewHolder) viewHolder).viewForeground;
 
         getDefaultUIUtil().onDraw(c, recyclerView, foregroundView, dX, dY,
                 actionState, isCurrentlyActive);

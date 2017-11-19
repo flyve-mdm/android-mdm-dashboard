4b8c377434f10f7babf5a5fe00ef6b13f6b6714b
Rafa Hernandez <rhernandez@teclib.com>
2017-11-19 19:51:17 -0400

    refactor(file): add swipe to delete

--- app/src/main/java/org/flyve/admin/dashboard/FileFragment.java
+++ app/src/main/java/org/flyve/admin/dashboard/FileFragment.java
@@ -1,15 +1,21 @@
 package org.flyve.admin.dashboard;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
 import org.flyve.admin.dashboard.adapter.FileAdapter;
+import org.flyve.admin.dashboard.adapter.FileTouchHelper;
 import org.flyve.admin.dashboard.utils.FlyveLog;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -18,11 +24,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class FileFragment extends Fragment {
 
     private ProgressBar pb;
     private RecyclerView lst;
+    private List<HashMap<String, String>> data;
+    private FileAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,6 +45,51 @@ public class FileFragment extends Fragment {
 
         LinearLayoutManager llm = new LinearLayoutManager(FileFragment.this.getActivity());
         lst.setLayoutManager(llm);
+
+        lst.setItemAnimator(new DefaultItemAnimator());
+        lst.addItemDecoration(new DividerItemDecoration(FileFragment.this.getContext(), DividerItemDecoration.VERTICAL));
+
+        // adding item touch helper
+        // only ItemTouchHelper.LEFT added to detect Right to Left swipe
+        // if you want both Right -> Left and Left -> Right
+        // add pass ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT as param
+        ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new FileTouchHelper(0, ItemTouchHelper.LEFT, new FileTouchHelper.RecyclerItemTouchHelperListener() {
+
+            /**
+             * callback when recycler view is swiped
+             * item will be removed on swiped
+             * undo option will be provided in snackbar to restore the item
+             */
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position) {
+                if (viewHolder instanceof FileAdapter.DataViewHolder) {
+                    // get the removed item name to display it in snack bar
+                    String name = data.get(viewHolder.getAdapterPosition()).get("name");
+
+                    // backup of removed item for undo purpose
+                    final HashMap<String, String> deletedItem = data.get(viewHolder.getAdapterPosition());
+                    final int deletedIndex = viewHolder.getAdapterPosition();
+
+                    // remove the item from recycler view
+                    mAdapter.removeItem(viewHolder.getAdapterPosition());
+
+                    // showing snack bar with Undo option
+                    Snackbar snackbar = Snackbar
+                            .make(FileFragment.this.getView(), " removed from list", Snackbar.LENGTH_LONG);
+                    snackbar.setAction("UNDO", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                            // undo is selected, restore the deleted item
+                            mAdapter.restoreItem(deletedItem, deletedIndex);
+                        }
+                    });
+                    snackbar.setActionTextColor(Color.YELLOW);
+                    snackbar.show();
+                }
+            }
+        });
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(lst);
 
         load(loadJSONFromAsset());
 
@@ -60,7 +114,7 @@ public class FileFragment extends Fragment {
 
     public void load(String jsonStr) {
 
-        ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
+        data = new ArrayList<>();
 
         try {
             JSONObject json = new JSONObject(jsonStr);
@@ -82,7 +136,7 @@ public class FileFragment extends Fragment {
 
             pb.setVisibility(View.GONE);
 
-            FileAdapter mAdapter = new FileAdapter(data);
+            mAdapter = new FileAdapter(data);
             lst.setAdapter(mAdapter);
 
         } catch (Exception ex) {
---
+++ app/src/main/java/org/flyve/admin/dashboard/adapter/FileTouchHelper.java
@@ -0,0 +1,67 @@
+package org.flyve.admin.dashboard.adapter;
+
+import android.graphics.Canvas;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+
+public class DeviceTouchHelper extends ItemTouchHelper.SimpleCallback {
+    private RecyclerItemTouchHelperListener listener;
+
+    public DeviceTouchHelper(int dragDirs, int swipeDirs, RecyclerItemTouchHelperListener listener) {
+        super(dragDirs, swipeDirs);
+        this.listener = listener;
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+        return true;
+    }
+
+    @Override
+    public void onSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState) {
+        if (viewHolder != null) {
+            final View foregroundView = ((DeviceAdapter.DataViewHolder) viewHolder).viewForeground;
+            getDefaultUIUtil().onSelected(foregroundView);
+        }
+    }
+
+    @Override
+    public void onChildDrawOver(Canvas c, RecyclerView recyclerView,
+                                RecyclerView.ViewHolder viewHolder, float dX, float dY,
+                                int actionState, boolean isCurrentlyActive) {
+        final View foregroundView = ((DeviceAdapter.DataViewHolder) viewHolder).viewForeground;
+        getDefaultUIUtil().onDrawOver(c, recyclerView, foregroundView, dX, dY,
+                actionState, isCurrentlyActive);
+    }
+
+    @Override
+    public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        final View foregroundView = ((DeviceAdapter.DataViewHolder) viewHolder).viewForeground;
+        getDefaultUIUtil().clearView(foregroundView);
+    }
+
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView,
+                            RecyclerView.ViewHolder viewHolder, float dX, float dY,
+                            int actionState, boolean isCurrentlyActive) {
+        final View foregroundView = ((DeviceAdapter.DataViewHolder) viewHolder).viewForeground;
+
+        getDefaultUIUtil().onDraw(c, recyclerView, foregroundView, dX, dY,
+                actionState, isCurrentlyActive);
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+        listener.onSwiped(viewHolder, direction, viewHolder.getAdapterPosition());
+    }
+
+    @Override
+    public int convertToAbsoluteDirection(int flags, int layoutDirection) {
+        return super.convertToAbsoluteDirection(flags, layoutDirection);
+    }
+
+    public interface RecyclerItemTouchHelperListener {
+        void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position);
+    }
+}

220fd34ea01378291fcbd5e50e0392a1298fff02
Rafa Hernandez <rhernandez@teclib.com>
2017-11-19 19:38:30 -0400

    refactor(device): add swipe delete

--- app/src/main/java/org/flyve/admin/dashboard/DeviceFragment.java
+++ app/src/main/java/org/flyve/admin/dashboard/DeviceFragment.java
@@ -1,28 +1,35 @@
 package org.flyve.admin.dashboard;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
-
 import org.flyve.admin.dashboard.adapter.DeviceAdapter;
+import org.flyve.admin.dashboard.adapter.DeviceTouchHelper;
 import org.flyve.admin.dashboard.utils.FlyveLog;
 import org.json.JSONArray;
 import org.json.JSONObject;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class DeviceFragment extends Fragment {
 
     private ProgressBar pb;
     private RecyclerView lst;
+    private List<HashMap<String, String>> data;
+    private DeviceAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,6 +43,52 @@ public class DeviceFragment extends Fragment {
 
         LinearLayoutManager llm = new LinearLayoutManager(DeviceFragment.this.getActivity());
         lst.setLayoutManager(llm);
+
+        lst.setItemAnimator(new DefaultItemAnimator());
+        lst.addItemDecoration(new DividerItemDecoration(DeviceFragment.this.getContext(), DividerItemDecoration.VERTICAL));
+
+        // adding item touch helper
+        // only ItemTouchHelper.LEFT added to detect Right to Left swipe
+        // if you want both Right -> Left and Left -> Right
+        // add pass ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT as param
+        ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new DeviceTouchHelper(0, ItemTouchHelper.LEFT, new DeviceTouchHelper.RecyclerItemTouchHelperListener() {
+
+            /**
+             * callback when recycler view is swiped
+             * item will be removed on swiped
+             * undo option will be provided in snackbar to restore the item
+             */
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position) {
+                if (viewHolder instanceof DeviceAdapter.DataViewHolder) {
+                    // get the removed item name to display it in snack bar
+                    String name = data.get(viewHolder.getAdapterPosition()).get("UserRealName");
+
+                    // backup of removed item for undo purpose
+                    final HashMap<String, String> deletedItem = data.get(viewHolder.getAdapterPosition());
+                    final int deletedIndex = viewHolder.getAdapterPosition();
+
+                    // remove the item from recycler view
+                    mAdapter.removeItem(viewHolder.getAdapterPosition());
+
+                    // showing snack bar with Undo option
+                    Snackbar snackbar = Snackbar
+                            .make(DeviceFragment.this.getView(), name + " removed from list", Snackbar.LENGTH_LONG);
+                    snackbar.setAction("UNDO", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                            // undo is selected, restore the deleted item
+                            mAdapter.restoreItem(deletedItem, deletedIndex);
+                        }
+                    });
+                    snackbar.setActionTextColor(Color.YELLOW);
+                    snackbar.show();
+                }
+            }
+        });
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(lst);
+
 
         load(loadJSONFromAsset());
 
@@ -60,7 +113,7 @@ public class DeviceFragment extends Fragment {
 
     public void load(String jsonStr) {
 
-        ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
+        data = new ArrayList<>();
 
         try {
             JSONObject json = new JSONObject(jsonStr);
@@ -89,7 +142,7 @@ public class DeviceFragment extends Fragment {
 
             pb.setVisibility(View.GONE);
 
-            DeviceAdapter mAdapter = new DeviceAdapter(data);
+            mAdapter = new DeviceAdapter(data);
             lst.setAdapter(mAdapter);
 
         } catch (Exception ex) {

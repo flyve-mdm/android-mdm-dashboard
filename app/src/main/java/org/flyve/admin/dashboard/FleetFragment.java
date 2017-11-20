8fb655cbef557053cfb89e67434c5b458e250f44
Rafa Hernandez <rhernandez@teclib.com>
2017-11-19 20:37:11 -0400

    refactor(fleet): add swipe to delete

--- app/src/main/java/org/flyve/admin/dashboard/FleetFragment.java
+++ app/src/main/java/org/flyve/admin/dashboard/FleetFragment.java
@@ -1,15 +1,21 @@
 package org.flyve.admin.dashboard;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
 import org.flyve.admin.dashboard.adapter.FleetAdapter;
+import org.flyve.admin.dashboard.adapter.FleetTouchHelper;
 import org.flyve.admin.dashboard.utils.FlyveLog;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -18,24 +24,72 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class FleetFragment extends Fragment {
 
     private ProgressBar pb;
     private RecyclerView lst;
+    private List<HashMap<String, String>> data;
+    private FleetAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_fleet, container, false);
 
-        pb = (ProgressBar) v.findViewById(R.id.pb);
+        pb = v.findViewById(R.id.pb);
         pb.setVisibility(View.VISIBLE);
 
         lst = v.findViewById(R.id.lst);
 
         LinearLayoutManager llm = new LinearLayoutManager(FleetFragment.this.getActivity());
         lst.setLayoutManager(llm);
+
+        lst.setItemAnimator(new DefaultItemAnimator());
+        lst.addItemDecoration(new DividerItemDecoration(FleetFragment.this.getContext(), DividerItemDecoration.VERTICAL));
+
+        // adding item touch helper
+        // only ItemTouchHelper.LEFT added to detect Right to Left swipe
+        // if you want both Right -> Left and Left -> Right
+        // add pass ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT as param
+        ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new FleetTouchHelper(0, ItemTouchHelper.LEFT, new FleetTouchHelper.RecyclerItemTouchHelperListener() {
+
+            /**
+             * callback when recycler view is swiped
+             * item will be removed on swiped
+             * undo option will be provided in snackbar to restore the item
+             */
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position) {
+                if (viewHolder instanceof FleetAdapter.DataViewHolder) {
+                    // get the removed item name to display it in snack bar
+                    String name = data.get(viewHolder.getAdapterPosition()).get("name");
+
+                    // backup of removed item for undo purpose
+                    final HashMap<String, String> deletedItem = data.get(viewHolder.getAdapterPosition());
+                    final int deletedIndex = viewHolder.getAdapterPosition();
+
+                    // remove the item from recycler view
+                    mAdapter.removeItem(viewHolder.getAdapterPosition());
+
+                    // showing snack bar with Undo option
+                    Snackbar snackbar = Snackbar
+                            .make(FleetFragment.this.getView(), name + " removed from list", Snackbar.LENGTH_LONG);
+                    snackbar.setAction("UNDO", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                            // undo is selected, restore the deleted item
+                            mAdapter.restoreItem(deletedItem, deletedIndex);
+                        }
+                    });
+                    snackbar.setActionTextColor(Color.YELLOW);
+                    snackbar.show();
+                }
+            }
+        });
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(lst);
 
         load(loadJSONFromAsset());
 
@@ -60,7 +114,7 @@ public class FleetFragment extends Fragment {
 
     public void load(String jsonStr) {
 
-        ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
+        data = new ArrayList<>();
 
         try {
             JSONObject json = new JSONObject(jsonStr);
@@ -79,7 +133,7 @@ public class FleetFragment extends Fragment {
 
             pb.setVisibility(View.GONE);
 
-            FleetAdapter mAdapter = new FleetAdapter(data);
+            mAdapter = new FleetAdapter(data);
             lst.setAdapter(mAdapter);
 
         } catch (Exception ex) {

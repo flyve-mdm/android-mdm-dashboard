0d6e120d62f9b469e16ffb2cfd9fe0ee7e523dea
Rafa Hernandez <rhernandez@teclib.com>
2017-11-21 20:51:20 -0400

    refactor(file): add detail activity

--- app/src/main/java/org/flyve/admin/dashboard/ui/FileDetailActivity.java
+++ app/src/main/java/org/flyve/admin/dashboard/ui/FileDetailActivity.java
@@ -1,7 +1,11 @@
 package org.flyve.admin.dashboard.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import org.flyve.admin.dashboard.R;
+import org.flyve.admin.dashboard.utils.FlyveLog;
 
 public class FileDetailActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class FileDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_detail);
+
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            try {
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            } catch(Exception ex) {
+                FlyveLog.e(ex.getMessage());
+            }
+
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onBackPressed();
+                }
+            });
+        }
+
     }
 }

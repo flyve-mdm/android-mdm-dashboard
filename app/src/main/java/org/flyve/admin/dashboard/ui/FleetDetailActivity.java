42b32564fdf5cae858d14f3adeace9fb6c6b87db
Rafa Hernandez <rhernandez@teclib.com>
2017-11-21 21:18:09 -0400

    build(manifest): add fleet activity

---
+++ app/src/main/java/org/flyve/admin/dashboard/ui/FleetDetailActivity.java
@@ -0,0 +1,13 @@
+package org.flyve.admin.dashboard.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FleetDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fleet_detail);
+    }
+}

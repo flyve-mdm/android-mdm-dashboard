b9e90ac7e019c4a9e791536031b0cccf08ec7303
Rafa Hernandez <rhernandez@teclib.com>
2017-11-21 20:34:45 -0400

    build(manifest): add device detail activity

---
+++ app/src/main/java/org/flyve/admin/dashboard/ui/DeviceDetailActivity.java
@@ -0,0 +1,13 @@
+package org.flyve.admin.dashboard.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeviceDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_detail);
+    }
+}

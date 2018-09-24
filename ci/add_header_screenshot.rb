eb242c197cb4cb39a9cc3c44c2550ea865a4b611
Naylin Medina <nmedina@teclib.com>
2018-09-24 09:32:51 -0400

    docs(headers): update license in ci folder
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- ci/add_header_screenshot.rb
+++ ci/add_header_screenshot.rb
@@ -1,28 +1,28 @@
 #
-#  Copyright (C) 2017 Teclib'
+#  LICENSE
+# 
+#  This file is part of Flyve MDM Admin Dashboard for Android.
 #
-#  This file is part of Flyve MDM Android.
-#
-#  Flyve MDM Android is a subproject of Flyve MDM. Flyve MDM is a mobile
-#  device management software.
-#
-#  Flyve MDM Android is free software: you can redistribute it and/or
+#  Admin Dashboard for Android is a subproject of Flyve MDM.
+#  Flyve MDM is a mobile device management software.
+# 
+#  Flyve MDM is free software: you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
 #  as published by the Free Software Foundation; either version 3
 #  of the License, or (at your option) any later version.
 #
-#  Flyve MDM Android is distributed in the hope that it will be useful,
+#  Flyve MDM is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  ------------------------------------------------------------------------------
-#  @author    Rafael Hernandez - rafaelje
-#  @copyright Copyright (c) 2017 Flyve MDM
+#  -------------------------------------------------------------------
+#  @author    Rafael Hernandez - <rhernandez@teclib.com>
+#  @copyright Copyright Teclib. All rights reserved.
 #  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
-#  @link      https://github.com/flyve-mdm/flyve-mdm-android-admin-dashboard
-#  @link      http://www.glpi-project.org/
-#  @link      https://flyve-mdm.com/
-#  ------------------------------------------------------------------------------
+#  @link      https://github.com/flyve-mdm/android-mdm-dashboard/
+#  @link      http://flyve.org/android-mdm-dashboard/
+#  @link      https://flyve-mdm.com
+#  -------------------------------------------------------------------
 #
 
 # add screenshots header
